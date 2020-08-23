7edec142f6c607ec6d553b45ad1866f2a6e09958
luokui <luokui@mogu.com>
2020-08-23 18:33:01 +0800

    队列

---
+++ src/leetcode/数据结构/队列/L25_Solution.java
@@ -0,0 +1,89 @@
+package leetcode.数据结构.队列;
+
+import common.ListNode;
+
+/**
+ * 描述:
+ *
+ * @author luokui
+ * @create 2020-08-23 17:15
+ */
+public class L25_Solution {
+
+    /**
+     * 递归反转列表 206
+     *
+     * @param head
+     * @return
+     */
+    ListNode reverse1(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode n = reverse(head.next);
+        head.next.next = head;
+        head.next = null;
+        return n;
+    }
+
+
+    /**
+     * 反转列表
+     *
+     * @param head
+     * @return
+     */
+    ListNode reverse(ListNode head) {
+        if (head == null) return null;
+        ListNode pre = null;
+        ListNode cur = head;
+        ListNode next = null;
+        while (cur != null) {
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+
+    /**
+     * 「反转以 a 为头结点的链表」其实就是「反转 a 到 null 之间的结点」，
+     * 那么如果让你「反转 a 到 b 之间的结点」
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    ListNode reverse(ListNode a, ListNode b) {
+        ListNode pre = null;
+        ListNode cur = a;
+        ListNode next = null;
+        while (cur != b) {
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        //新的节点连接起来
+        //a.next = b;
+        return pre;
+    }
+
+
+    ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null) return null;
+        ListNode a = head;
+        ListNode b = head;
+        for (int i = 0; i < k; i++) {
+            if (b == null) {
+                return a;
+            }
+            b = b.next;
+        }
+        ListNode newHead = reverse(a, b);
+        a.next = reverseKGroup(b, k);
+        return newHead;
+
+
+    }
+
+}
---
+++ src/leetcode/数据结构/队列/L92_Solution.java
@@ -0,0 +1,57 @@
+package leetcode.数据结构.队列;
+
+import common.ListNode;
+
+/**
+ * 描述:
+ * <p>
+ * 反转从位置 m 到 n 的链表。请使用一趟扫描完成反转。
+ * <p>
+ * 说明:
+ * 1 ≤ m ≤ n ≤ 链表长度。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: 1->2->3->4->5->NULL, m = 2, n = 4
+ * 输出: 1->4->3->2->5->NULL
+ *
+ * @author luokui
+ * @create 2020-06-23 17:56
+ */
+public class L92_Solution {
+
+
+    ListNode reverse(ListNode a, int k) {
+        ListNode pre = null;
+        ListNode cur = a;
+        ListNode next = null;
+        while (k >= 0) {
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+            k--;
+        }
+        //新的节点连接起来
+        a.next = cur;
+        return pre;
+    }
+
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        if (head == null || m == n) return head;
+        ListNode mPre = head;
+        ListNode mn = head;
+        for (int i = 1; i < m; i++) {
+            mPre = mn;
+            mn = mn.next;
+        }
+        //这地方比较绕，可以优化
+        if (mPre == mn) {
+            head = reverse(mn, n - m);
+        } else {
+            mPre.next = reverse(mn, n - m);
+        }
+        return head;
+
+    }
+}
