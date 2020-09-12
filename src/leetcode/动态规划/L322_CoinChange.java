d604c710e431289e688c87dd6d19a38fc30de5e8
luokui <luokui@mogu.com>
2020-09-12 19:48:03 +0800

    动态规划

--- src/leetcode/动态规划/L322_CoinChange.java
+++ src/leetcode/动态规划/L322_CoinChange.java
@@ -30,8 +30,22 @@ public class L322_CoinChange {
             if (coins == null || coins.length == 0 || amount <= 0) {
                 return 0;
             }
-            return 0;
+            int[] dp = new int[amount + 1];
+            for (int i = 0; i <= amount; i++) {
+                dp[i] = i + 1;
+            }
+            dp[0] = 0;
+            for (int i = 1; i <= amount; i++) {
+                for (int j = 0; j < coins.length; j++) {
+                    if (i >= coins[j]) {
+                        //这个条件说明小的容量也无解
+                        if (dp[i - coins[j]] == i - coins[j] + 1) continue;
+                        dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                    }
 
+                }
+            }
+            return dp[amount] == amount + 1 ? -1 : dp[amount];
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
