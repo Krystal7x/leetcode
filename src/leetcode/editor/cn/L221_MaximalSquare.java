d3fee13a3acf0d5b5816282fc7c72110f1e6488b
luokui <luokui@mogu.com>
2020-09-20 18:03:18 +0800

    动态规划

---
+++ src/leetcode/editor/cn/L221_MaximalSquare.java
@@ -0,0 +1,34 @@
+package leetcode.editor.cn;
+
+//在一个由 0 和 1 组成的二维矩阵内，找到只包含 1 的最大正方形，并返回其面积。 
+//
+// 示例: 
+//
+// 输入: 
+//
+//1 0 1 0 0
+//1 0 1 1 1
+//1 1 1 1 1
+//1 0 0 1 0
+//
+//输出: 4 
+// Related Topics 动态规划 
+// 👍 561 👎 0
+
+
+public class L221_MaximalSquare {
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int maximalSquare(char[][] matrix) {
+            return 0;
+
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+    public static void main(String[] args) {
+        Solution solution = new L221_MaximalSquare().new Solution();
+
+    }
+}
