49bf056dec492a34947fad93ccb875223e135b69
luokui <luokui@mogu.com>
2020-09-26 17:35:15 +0800

    矩阵动态规划

--- src/leetcode/动态规划/L1277_CountSquareSubmatricesWithAllOnes.java
+++ src/leetcode/动态规划/L1277_CountSquareSubmatricesWithAllOnes.java
@@ -1,4 +1,4 @@
-package leetcode.editor.cn;
+package leetcode.动态规划;
 
 //给你一个 m * n 的矩阵，矩阵中的元素不是 0 就是 1，请你统计并返回其中完全由 1 组成的 正方形 子矩阵的个数。 
 //
@@ -52,8 +52,34 @@ public class L1277_CountSquareSubmatricesWithAllOnes {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int countSquares(int[][] matrix) {
-            return 0;
+            if (matrix == null || matrix.length == 0) return 0;
+            int row = matrix.length;
+            int col = matrix[0].length;
+            int[][] dp = new int[row][col];
+            int count = 0;
+            for (int i = 0; i < row; i++) {
+                dp[i][0] = matrix[i][0];
+            }
+            for (int i = 0; i < col; i++) {
+                dp[0][i] = matrix[0][i];
+            }
+            for (int i = 1; i < row; i++) {
+                for (int j = 1; j < col; j++) {
+                    if (matrix[i][j] == 0) {
+                        continue;
+                    }
+                    if (matrix[i][j] == 1) {
+                        dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                    }
+                }
 
+            }
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < col; j++) {
+                    count = count + dp[i][j];
+                }
+            }
+            return count;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
