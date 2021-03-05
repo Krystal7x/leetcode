1d6b9a7a306b830fac9a2bebd4a4b315fed54b90
luokui <luokui@mogu.com>
2021-03-05 17:58:39 +0800

    备注

--- src/sword/S43_OneNzhengShuZhong1chuXianDeCiShuLcof.java
+++ src/sword/S43_OneNzhengShuZhong1chuXianDeCiShuLcof.java
@@ -39,17 +39,23 @@ public class S43_OneNzhengShuZhong1chuXianDeCiShuLcof {
             if (n == 0) return 0;
             int digit = (int) Math.log10(n) + 1;
             int[][] dp = new int[digit + 1][2];
+            //到个位的出现次数
             dp[1][0] = n % 10 >= 1 ? 1 : 0;
+            //到9为止的出现次数
             dp[1][1] = 1;
             for (int i = 2; i <= digit; i++) {
+                //取第n位数字
                 int k = n / (int) (Math.pow(10, i - 1)) % 10;
+                //1.1、*9出现几次 加  后几位出现几次
                 dp[i][0] = k * dp[i - 1][1] + dp[i - 1][0];
+                //1.2、再加上 n位 的k出现次数
                 if (k == 1) {
                     dp[i][0] += n % (int) (Math.pow(10, i - 1)) + 1;
                 }
                 if (k > 1) {
                     dp[i][0] += Math.pow(10, i - 1);
                 }
+                //3、* 算9的次数
                 dp[i][1] = 10 * dp[i - 1][1] + (int) Math.pow(10, i - 1);
 
             }
